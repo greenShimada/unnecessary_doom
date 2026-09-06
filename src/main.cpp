82a593e8eeae03631da74799fed55cb28773c5d8
greenShimada <matheus_jonnas@proton.me>
2026-09-06 12:15:16 -0300

    removed gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,58 @@
-#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <cstdio>
+#include <glad/gl.h>
+
+void processInput(GLFWwindow *);
 
 int main() {
-    if (!glfwInit()) {
-        fprintf(stderr, "Error initializing GLFW\nTry to install it maybe?");
-        return -1;
-    }
+  if (!glfwInit()) {
+    fprintf(stderr, "Error initializing GLFW\nTry to install it maybe?");
+    return -1;
+  }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Unnecessary Doom", NULL, NULL);
-    if (!window) {
-        fprintf(stderr, "Error creating the window\n");
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoaderLoadGL()) {
-        fprintf(stderr, "Error loading GLAD");
-        return -1;
-    }
-
-
-    // game loop
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Unnecessary Doom", NULL, NULL);
+  if (!window) {
+    fprintf(stderr, "Error creating the window\n");
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoaderLoadGL()) {
+    fprintf(stderr, "Error loading GLAD");
+    return -1;
+  }
+
+  // game loop
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                        0.0f,  0.0f,  0.5f, 0.0f};
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
